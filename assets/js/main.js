bfd4114cf1662a20bde3c027c24847c55d212ac1
Clément Galidie <clement-galidie@cifacom.com>
2014-10-15 14:58:17 +0200

    Add dropdown menu and some images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 (function () {
 	'use strict';
-	
+	var openSubMenuActions = document.querySelector('.listActions-link--more');
+	var subMenuActions = document.querySelector('.listActionsSub');
+
+	openSubMenuActions.addEventListener('click', function (e) {
+		subMenuActions.classList.toggle('listActionsSub--show');
+		e.preventDefault();
+	});
 }());
