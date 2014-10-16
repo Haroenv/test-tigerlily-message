d028a7a6838abc89d59751eac3953aae2e434c0d
Clement Galidie <galidie.clement@gmail.com>
2014-10-16 02:19:06 +0200

    Modification du readme, version responsive, script toggle reponses, polyfills IE9

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Test *Tigerlily message*
+
+Ma réponse au test **Tigerlily message**
+
+## Caractéristiques
+
+* Codes HTML et CSS valides.
+* Possibilité de naviguer en n'utilisant que le clavier.
+* Responsive.
+* JavaScript *Vanilla* (pas de jQuery).
+* Utilisation de [normalize.css](http://necolas.github.io/normalize.css/).
+* Utilisation des unités *rem* et *em*.
+* Syntaxe *BEM* (Block Element Modifier).
+
+Testé avec succès sous les navigateurs suivants :
+
+* Google Chrome Canary v.40.0 ;
+* Google Chrome v.38.0 ;
+* Internet Explorer 11, 10 et 9 ;
+* Opera  v.25.0 ;
+* Safari v.5.1.7.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,27 @@
+// IEF + mode stricte.
 (function () {
 	'use strict';
+	/*
+	 * Script permettant d'ouvrir et de fermer le menu déroulant more
+	 * lors du clic.
+	 */
 	var openSubMenuActions = document.querySelector('.listActions-link--more');
 	var subMenuActions = document.querySelector('.listActionsSub');
 
 	openSubMenuActions.addEventListener('click', function (e) {
 		subMenuActions.classList.toggle('listActionsSub--show');
-		e.preventDefault();
+		e.preventDefault(); // Empêcher la redirection.
 	});
+
+	/*
+	 * Script permettant d'ouvrir et de fermer la zone contenant
+	 * les réponses au tweet principal lors du clic.
+	 */
+	var openAnswerTweetWrapper = document.querySelector('.listActions-link--showComment');
+	var answerTweetWrapper =  document.querySelector('.answers');
+
+	openAnswerTweetWrapper.addEventListener('click', function (e) {
+		answerTweetWrapper.classList.toggle('answers--show');
+		e.preventDefault(); // Empêcher la redirection.
+	})
 }());
